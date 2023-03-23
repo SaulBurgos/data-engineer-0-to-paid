55960c36fbb0f78553ce64b75d6bd8edde60f4e7
saul.burgos <saul.burgos@ooqia.com>
2023-03-23 16:24:33 -0600

    base file

--- elt.py
+++ elt.py
@@ -1,1 +1,21 @@
 # empty
+# Constants
+SOURCE_DB = "source_db"
+DESTINATION_DB = "destination_db"
+TABLE_NAME = "table_name"
+DATE_COLUMN = "date_column"
+SALES_COLUMN = "sales_column"
+
+# Code to extract data from the source database
+def extract_data(table_name):
+    pass
+
+
+# Code to transform the data
+def transform_data(data_frame):
+    pass
+
+
+# Code to load the data into the destination database
+def load_data(data_frame):
+    pass
